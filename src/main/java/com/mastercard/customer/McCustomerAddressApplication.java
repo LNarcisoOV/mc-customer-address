217c1fde2f791dc95dd45d22c8aaee714331c081
Leonardo Narciso <LNarcisoOV@gmail.com>
2022-05-16 13:20:28 +0100

    First commit.

---
+++ src/main/java/com/mastercard/customer/McCustomerAddressApplication.java
@@ -0,0 +1,13 @@
+package com.mastercard.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McCustomerAddressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McCustomerAddressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mastercard/customer/McCustomerAddressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mastercard.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McCustomerAddressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

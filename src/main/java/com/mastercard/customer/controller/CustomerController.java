82c169c8f0b6fe9d4ab83699688c960ba60286d8
Leonardo Narciso <LNarcisoOV@gmail.com>
2022-07-22 09:55:53 +0000

    Add log.

--- src/main/java/com/mastercard/customer/controller/CustomerController.java
+++ src/main/java/com/mastercard/customer/controller/CustomerController.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +26,8 @@ import com.mastercard.customer.service.CustomerService;
 @RequestMapping("/customer")
 public class CustomerController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomerController.class);
+    
     @Autowired
     private CustomerService customerService;
 
@@ -37,9 +41,11 @@ public class CustomerController {
     public ResponseEntity<CustomerDTO> getById(@PathVariable Long id) {
         Optional<Customer> customerOpt = customerService.getById(id);
         if (customerOpt.isPresent()) {
+            LOGGER.info("Customer get by id: {} ", id);
             CustomerDTO customerDTO = modelMapper.map(customerOpt.get(), CustomerDTO.class);
             return new ResponseEntity<CustomerDTO>(customerDTO, HttpStatus.OK);
         } else {
+            LOGGER.warn("Customer not found.");
             return new ResponseEntity<CustomerDTO>(HttpStatus.NOT_FOUND);
         }
     }
@@ -49,13 +55,16 @@ public class CustomerController {
         try {
             Optional<Customer> customerOpt = customerService.save(customerDTO);
             if (customerOpt.isPresent()) {
+                LOGGER.info("Created customer: {} ", customerOpt.get().toString());
                 CustomerDTO customerDTOResponse =
                         modelMapper.map(customerOpt.get(), CustomerDTO.class);
                 return new ResponseEntity<CustomerDTO>(customerDTOResponse, HttpStatus.CREATED);
             } else {
+                LOGGER.warn("Error to create customer.");
                 return new ResponseEntity<CustomerDTO>(HttpStatus.INTERNAL_SERVER_ERROR);
             }
         } catch (RuntimeException runtimeException) {
+            LOGGER.error("Error to create customer: {} ", runtimeException.getMessage());
             throw new RuntimeException("An exception occurs;");
         }
     }
@@ -64,10 +73,12 @@ public class CustomerController {
     public ResponseEntity<List<AddressDTO>> getAddressById(@PathVariable Long customerId) {
         List<Address> adresses = addressService.getById(customerId);
         if (!adresses.isEmpty()) {
+            LOGGER.info("Get customer's address by customer id: {} ", customerId);
             List<AddressDTO> addressesDTO =
                     Arrays.asList(modelMapper.map(adresses, AddressDTO[].class));
             return new ResponseEntity<List<AddressDTO>>(addressesDTO, HttpStatus.OK);
         } else {
+            LOGGER.warn("Customer's address not found for the id: {} ", customerId);
             return new ResponseEntity<List<AddressDTO>>(HttpStatus.NOT_FOUND);
         }
     }
@@ -78,12 +89,15 @@ public class CustomerController {
         try {
             Optional<Address> addressOpt = addressService.save(customerId, adressDTO);
             if (addressOpt.isPresent()) {
+                LOGGER.info("Created customer's address for the id: {} ", customerId);
                 AddressDTO addressDTOResponse = modelMapper.map(addressOpt.get(), AddressDTO.class);
                 return new ResponseEntity<AddressDTO>(addressDTOResponse, HttpStatus.CREATED);
             } else {
+                LOGGER.warn("Error to create address for customer id: {} ", customerId);
                 return new ResponseEntity<AddressDTO>(HttpStatus.INTERNAL_SERVER_ERROR);
             }
         } catch (RuntimeException runtimeException) {
+            LOGGER.error("Error to create address for customer id: {} ", customerId);
             throw new RuntimeException(runtimeException.getMessage());
         }
     }
